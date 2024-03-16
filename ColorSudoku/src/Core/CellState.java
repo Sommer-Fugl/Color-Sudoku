b44e40dd5672e37c6eca23a544b7a3c395224137
Nikita Buinovskyi <nikita.buinovskyi@student.tuke.sk>
2024-03-16 17:54:22 +0100

    new initial

---
+++ ColorSudoku/src/Core/CellState.java
@@ -0,0 +1,13 @@
+package Core;
+public enum CellState {
+    EMPTY,
+    GENERATED;
+
+    public static CellState getCellState(int code){
+        for(CellState c: CellState.values()){
+            if(c.ordinal() == code)
+                return c;
+        }
+        return null;
+    }
+}
